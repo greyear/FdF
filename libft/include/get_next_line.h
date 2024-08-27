4fd783e2e87dcd02a445225e072cfd3772bc6fbb
azinchen <azinchen@student.hive.fi>
2024-08-27 18:10:57 +0300

    Initial commit

---
+++ libft/include/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azinchen <azinchen@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/13 15:36:01 by azinchen          #+#    #+#             */
+/*   Updated: 2024/08/10 19:45:44 by azinchen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin_stash(char *stash, char *buf);
+char	*ft_substr(char *stash, unsigned int start, size_t len);
+size_t	find_nl(char *stash);
+
+#endif
