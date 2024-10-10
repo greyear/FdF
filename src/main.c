5ac1f28e45138900374d899df8ad906b1747e600
azinchen <azinchen@student.hive.fi>
2024-10-10 20:37:44 +0300

    something

--- src/main.c
+++ src/main.c
@@ -21,49 +21,26 @@ int	main(int argc, char *argv[])
 	mlx_image_t		*image;
 	t_px_matrix		pixel_matrix;
 
-	clock_t time_start= clock(); 
+	
 	if (argc != 2 || !argv[1])
 		exit(EXIT_FAILURE);
-	
-	clock_t time_1 = clock() - time_start;
-   	printf("after argc check %f\n", (double)time_1 / CLOCKS_PER_SEC);
-
 	read = read_map(argv[1]);
-
-	clock_t time_2 = clock() - time_start;
-   	printf("after reading %f\n", (double)time_2 / CLOCKS_PER_SEC);
 
 	// TODO: checks of read
 	// TODO: if here function returned exit, will I have problems with memory?
 	iso_matrix = to_iso_matrix(read, M_PI / 6);
-
-	clock_t time_3 = clock() - time_start;
-   	printf("after iso_matrix creation %f\n", (double)time_3 / CLOCKS_PER_SEC);
-
 	if (!(mlx = mlx_init(1000, 1000, "FDF", true)))
 	{
 		puts(mlx_strerror(mlx_errno));
 		return (EXIT_FAILURE);
 	}
-
-	clock_t time_4 = clock() - time_start;
-   	printf("after mlx init %f\n", (double)time_4 / CLOCKS_PER_SEC);
-
 	if (!(image = mlx_new_image(mlx, 1000, 1000)))
 	{
 		mlx_close_window(mlx);
 		puts(mlx_strerror(mlx_errno));
 		return (EXIT_FAILURE);
 	}
-
-	clock_t time_5 = clock() - time_start;
-   	printf("after image creation %f\n", (double)time_5 / CLOCKS_PER_SEC);
-
 	pixel_matrix = to_px_matrix(iso_matrix);
-
-	clock_t time_6 = clock() - time_start;
-   	printf("after px_matrix creation %f\n", (double)time_6 / CLOCKS_PER_SEC);
-
 	put_px_matrix(image, pixel_matrix);
 
 	clock_t time_7 = clock() - time_start;
--- src/reading/read_map.c
+++ src/reading/read_map.c
@@ -25,6 +25,8 @@ t_read	*read_map(char *file_name)
 	t_color	color;
 	t_read	*first;
 	
+	clock_t time_start= clock(); 	
+
 	fd = open(file_name, O_RDONLY);
 	if (fd == -1)
 	{
@@ -32,6 +34,10 @@ t_read	*read_map(char *file_name)
 		perror("Error opening file");
 		exit(EXIT_FAILURE);
 	}
+
+	clock_t time_1 = clock() - time_start;
+   	printf("after opening file %f\n", (double)time_1 / CLOCKS_PER_SEC);
+
 	first = NULL;
 	y = 0;
 	while (1)
@@ -39,16 +45,32 @@ t_read	*read_map(char *file_name)
 		line = get_next_line(fd);
 		if (line == NULL)
 			break ;
+
+		clock_t time_2 = clock() - time_start;
+   		printf("after gnl %f\n", (double)time_2 / CLOCKS_PER_SEC);
+
 		point_info = ft_split(line, ' ');
 		//checks for point_info
 		free(line);
+
+		clock_t time_3 = clock() - time_start;
+   		printf("after splitting %f\n", (double)time_3 / CLOCKS_PER_SEC);
+
 		//printf("x:%d", x);
 		x = 0;
 		while (point_info[x] != NULL)
 		{
 			if (contains_comma(point_info[x]) == 1)
+
+		clock_t time_4 = clock() - time_start;
+   		printf("after comma check %f\n", (double)time_4 / CLOCKS_PER_SEC);
+
 			{
 				separate = ft_split(point_info[x], ',');
+
+				clock_t time_5 = clock() - time_start;
+   				printf("after splitting point info %f\n", (double)time_5 / CLOCKS_PER_SEC);
+
 				if (array_len(separate) != 2)
 					{
 						perror("Input data error: array_len");
@@ -64,6 +86,10 @@ t_read	*read_map(char *file_name)
 						perror("Input data error: color check");
 						exit(EXIT_FAILURE);
 					}
+				
+				clock_t time_6 = clock() - time_start;
+   				printf("after checks %f\n", (double)time_6 / CLOCKS_PER_SEC);
+
 				z = ft_atoi(separate[0]);
 				color = extract_rgba(ft_atoi_color(separate[1]), ft_strlen(separate[1]) - 2);
 				clean_arr(separate);
